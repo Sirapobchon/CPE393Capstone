5af9fa7af4ceeeadbbd6d5ff29c2b221bde91cc7
pppcyd <girrlwithluvv@gmail.com>
2024-04-08 02:13:08 +0700

    add Prevent scrolling/ leaderboard page/ new png

--- src/Components/Leaderboard/leaderboard.jsx
+++ src/Components/Leaderboard/leaderboard.jsx
@@ -1,4 +1,12 @@
 import React, { useState , useEffect } from 'react';
+import {useNavigate}  from "react-router-dom";
+import './leaderboard.css'
+import bg from '../Assets/element/leaderbg2.mp4'
+import BackBut from '../Assets/element/back.png'
+import id from '../Assets/element/id.png'
+import usr from '../Assets/element/usr.png'
+import score from '../Assets/element/score.png'
+import sort from '../Assets/element/sort.png'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
@@ -12,24 +20,31 @@ const Leaderboard = () => {
         .then((data) => setPlayers(data))
         .catch((error) => console.error('Error fetching players:', error));
     }, []); // Empty dependency array ensures this effect runs once when the component mounts
-  
-
+    
+  const navigate = useNavigate();
   // Function to sort players by score
   const sortPlayersByScore = () => {
     const sortedPlayers = [...players].sort((a, b) => b.score - a.score);
     setPlayers(sortedPlayers);
   };
+  const handleBack = () => {
+    navigate('/home');
+  }
 
   return (
-    <div>
-      <h2>Leaderboard</h2>
-      <button onClick={sortPlayersByScore}>Sort by Score</button>
+    <div className="page">
+      <video className="AnimationBg" src={bg} autoPlay muted loop></video> 
+      <button className="sort" onClick={sortPlayersByScore}>
+      <img src={sort}alt="sort button"></img> </button>
+      <button className="Backbut" onClick={handleBack}>
+                    <img src={BackBut}alt="Back button"></img>
+                </button>
       <table>
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Score</th>
+            <th className="Identify"> <img src={id}alt="id title"></img></th>
+            <th className="Name"><img src={usr}alt="username title"></img></th>
+            <th className="Score"><img src={score}alt="score title"></img></th>
           </tr>
         </thead>
         <tbody>
