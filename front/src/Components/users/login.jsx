8f06a748d973eb41a77b4eb5d307ab33312ccb1d
pppcyd <girrlwithluvv@gmail.com>
2024-04-08 23:16:35 +0700

    login without bg

--- front/src/Components/users/login.jsx
+++ front/src/Components/users/login.jsx
@@ -7,7 +7,10 @@ import Regisbut from "../Assets/element/Register.png";
 import Noacc from "../Assets/element/noacc.png";
 import BackBut from "../Assets/element/back.png";
 import LoginBoard from "../Assets/element/loginboard.png";
-
+import usrtt from "../Assets/element/username.png";
+import passtt from "../Assets/element/password.png";
+import logintt from "../Assets/logintt.png";
+import dirt from "../Assets/element/dirt.png";
 export default function Login() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
@@ -28,7 +31,10 @@ export default function Login() {
   };
   return (
     <div className="LoginPage">
+      <img className="logintt" src={logintt} alt="Login tt"></img>
       
+      <img className="usrtt" src={usrtt} alt="Login Board"></img>
+      <img className="passtt" src={passtt} alt="Login Board"></img>
       <img className="LoginBoard" src={LoginBoard} alt="Login Board"></img>
       <div>
         <div className="reciveUsername1">
@@ -50,7 +56,7 @@ export default function Login() {
           />
         </div>
 
-        <button className="loginbut" onClick={handleLogin}>
+        <button className="loginbut1" onClick={handleLogin}>
           <img src={loginbut} alt="Login Button"></img>
         </button>
         <p className="NoAcc">
@@ -59,10 +65,13 @@ export default function Login() {
         <button className="RegisBut1" onClick={handleRegister}>
           <img src={Regisbut} alt="Register button"></img>
         </button>
-        <button className="Backbut" onClick={handleBack}>
+        <button className="Backbut1" onClick={handleBack}>
           <img src={BackBut} alt="Back button"></img>
         </button>
       </div>
+     <img className="dirt1" src={dirt} alt="Login Board"></img>
+     <img className="dirt2" src={dirt} alt="Login Board"></img>
+     <img className="dirt3" src={dirt} alt="Login Board"></img>
     </div>
   );
 }
