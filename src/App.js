d35729ab9f121e647afc59d7cce98fe3778f0269
janekmutt <janesirikarn@gmail.com>
2024-04-08 18:07:22 +0700

    MainGamePage(Not finish)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import TicTacToe from './Components/TicTacToe/TicTacToe';
-import { Route, Routes } from "react-router-dom"
-import Login from './Components/users/login';
-import Header from './Components/header/header';
-import Register from './Components/users/register';
-import Leaderboard from './Components/Leaderboard/leaderboard';
-import Home from './Components/users/Home';
-import HowToPlay from './Components/users/HowToPlay';
+import "./App.css";
+import TicTacToe from "./Components/TicTacToe/TicTacToe";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Components/users/login";
+import Header from "./Components/header/header";
+import Register from "./Components/users/register";
+import Leaderboard from "./Components/Leaderboard/leaderboard";
+import Home from "./Components/users/Home";
+import MainGame from "./Components/users/MainGame";
+import HowToPlay from "./Components/users/HowToPlay";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<TicTacToe  />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/leaderboard' element={<Leaderboard />}></Route>
-        <Route path='/Home' element={<Home />}></Route>
-        <Route path='/HowToPlay' element={<HowToPlay />}></Route>
+        <Route path="/" element={<TicTacToe />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/leaderboard" element={<Leaderboard />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
+        <Route path="/MainGame" element={<MainGame />}></Route>
+        <Route path="/HowToPlay" element={<HowToPlay />}></Route>
       </Routes>
     </>
   );
