84f841532246008588a923d762edce1c3397a3ca
janekmutt <janesirikarn@gmail.com>
2024-04-09 00:08:07 +0700

    RegisterPage(element)

--- front/src/Components/users/register.jsx
+++ front/src/Components/users/register.jsx
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./register.css"; // eslint-disable-next-line
 // import MediaQuery, { useMediaQuery } from "react-responsive";
-import BGvid from "../Assets/registerbg.mp4";
+import BGvid from "../Assets/CloudBg.mp4";
+import registertt from "../Assets/element/registertt.png";
+import emailtt from "../Assets/element/email.png";
 import loginbut from "../Assets/element/Login.png";
+import usrtt from "../Assets/element/username.png";
+import passtt from "../Assets/element/password.png";
+import cfpasstt from "../Assets/element/cfpass.png";
 import signupbut from "../Assets/element/signup.png";
 import backbut from "../Assets/element/back.png";
-import clicksound from '../SFX/interface-button.mp3'
+import dirt from "../Assets/element/dirt.png";
+import RegisterBoard from "../Assets/element/smallerthanbiggerboard.png";
+import clicksound from "../SFX/interface-button.mp3";
+
 const clickaudio = new Audio(clicksound);
 
 function Register() {
@@ -38,31 +46,30 @@ function Register() {
     ) {
       const myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
-      
+
       const raw = JSON.stringify({
-        "username": username,
-        "email": email,
-        "password": password
+        username: username,
+        email: email,
+        password: password,
       });
-      
+
       const requestOptions = {
         method: "POST",
         headers: myHeaders,
         body: raw,
-        redirect: "follow"
+        redirect: "follow",
       };
-      
+
       await fetch("http://localhost:8080/api/users/register", requestOptions)
         .then((response) => response.json())
         .then((result) => {
           console.log(result);
-          if(result.success === 1){
+          if (result.success === 1) {
             alert(`Welcome! ${username} let go to login page.`);
-            navigate('/login')
-        }
-        else{
-          alert("register fail")
-        }
+            navigate("/login");
+          } else {
+            alert("register fail");
+          }
         })
         .catch((error) => console.error(error));
     } else {
@@ -70,11 +77,11 @@ function Register() {
     }
   };
   const handleLogin = () => {
-    clickaudio.play()
+    clickaudio.play();
     navigate("/login");
   };
   const handleBack = () => {
-    clickaudio.play()
+    clickaudio.play();
     navigate("/home");
   };
   //   const handleMediaQueryChange = (matches) => {
@@ -84,10 +91,21 @@ function Register() {
 
   return (
     <div className="RegisterPage">
+      <img className="registertt" src={registertt} alt="Register tt"></img>
       <video className="AnimationBg" src={BGvid} autoPlay muted loop></video>
+      <img className="emailtt" src={emailtt} alt="Email tt"></img>
+      <img className="usrtt" src={usrtt} alt="Username tt"></img>
+      <img className="passtt" src={passtt} alt="Password tt"></img>
+      <img className="cfpasstt" src={cfpasstt} alt="Confirm Password tt"></img>
+      <img
+        className="RegisterBoard"
+        src={RegisterBoard}
+        alt="Register Board"
+      ></img>
       <div>
         <div className="reciveEmail">
-          <input className="Textbox"
+          <input
+            className="Textbox"
             type="text"
             placeholder="email"
             required
@@ -97,7 +115,8 @@ function Register() {
           />
         </div>
         <div className="reciveUsername">
-          <input className="Textbox"
+          <input
+            className="Textbox"
             type="text"
             placeholder="username"
             required
@@ -107,7 +126,8 @@ function Register() {
           />
         </div>
         <div className="recivePassword">
-          <input className="Textbox"
+          <input
+            className="Textbox"
             type="password"
             placeholder="password"
             required
@@ -117,7 +137,8 @@ function Register() {
           />
         </div>
         <div className="reciveConfirmPassword">
-          <input className="Textbox"
+          <input
+            className="Textbox"
             type="password"
             placeholder="confirm password"
             required
@@ -127,16 +148,19 @@ function Register() {
           />
         </div>
 
-        <button className="BackBut1" onClick={handleLogin}>
+        <button className="loginbut1" onClick={handleLogin}>
           <img src={loginbut} alt="Login Button"></img>
         </button>
         <button className="RegisBut" onClick={handleRegister}>
           <img src={signupbut} alt="signup button"></img>
         </button>
-        <button className="BackBut" onClick={handleBack}>
+        <button className="BackBut2" onClick={handleBack}>
           <img src={backbut} alt="back button"></img>
         </button>
       </div>
+      <img className="dirt1" src={dirt} alt="Login Board"></img>
+      <img className="dirt2" src={dirt} alt="Login Board"></img>
+      <img className="dirt3" src={dirt} alt="Login Board"></img>
     </div>
   );
 }
