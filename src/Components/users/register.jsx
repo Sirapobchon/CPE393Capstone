027ca0f4a3a635448fdb3454f32927bddfe94aff
janekmutt <janesirikarn@gmail.com>
2024-04-08 15:07:55 +0700

    RegisterPage(not finish)

--- src/Components/users/register.jsx
+++ src/Components/users/register.jsx
@@ -1,50 +1,109 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import "./register.css"; // eslint-disable-next-line
+// import MediaQuery, { useMediaQuery } from "react-responsive";
+import BGvid from "../Assets/registerbg.mp4";
+import loginbut from "../Assets/element/Login.png";
+import signupbut from "../Assets/element/signup.png";
+import backbut from "../Assets/element/back.png";
 
 function Register() {
-    const navigate = useNavigate();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [rePassword, setRePassword] = useState("");
-    const [nickname, setNickname] = useState("");
-    const handleRegister = () => {
-        if(username !== "" && password !== "" && email !== "" &&  nickname !== "" &&  rePassword === password) {
-            alert(`Welcome! ${nickname} let go to login page.`);
-            navigate('/login');
-        }else{
-            alert('Please enter all infomation');
-        }
+  //   const isDesktopOrLaptop = useMediaQuery(
+  //     { minWidth: 1224 },
+  //     undefined,
+  //     (matches) => {
+  //       handleMediaQueryChange(matches);
+  //     }
+  //   );
+
+  //   const isTabletorMobile = useMediaQuery({ query: "(max-width: 800px)" });
+  //   const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  //   const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
+
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  // const [nickname, setNickname] = useState("");
+  const handleRegister = () => {
+    if (
+      username !== "" &&
+      password !== "" &&
+      email !== "" &&
+      // nickname !== "" &&
+      rePassword === password
+    ) {
+      alert(`Welcome! ${username} let go to login page.`);
+      navigate("/login");
+    } else {
+      alert("Please enter all infomation");
     }
-    return(
-        <div className="container mx-auto">
-            <div className="flex justify-center items-center mt-[10rem]">
-            <div className="border-4 border-solid border-cyan-400 rounded-md w-[20rem] bg-transparent px-4 flex items-center flex-col">
-                <h1 className=" text-white font-bold text-xl my-4">Register</h1>
-                <div className="my-2">
-                    <p className="text-white font-bold my-2">Username</p>
-                    <input type="text" className="border-2 border-solid px-2 border-black text-white bg-transparent outline-none rounded-md" placeholder="username" 
-                    required value={username} onChange={(e) => setUsername(e.target.value)}/>
-                    <p className="text-white font-bold my-2">Nickname</p>
-                    <input type="text" className="border-2 border-solid px-2 border-black text-white bg-transparent outline-none rounded-md" placeholder="nickname" 
-                    required value={nickname} onChange={(e) => setNickname(e.target.value)}/>
-                    <p className="text-white font-bold my-2">Email</p>
-                    <input type="text" className="border-2 border-solid px-2 border-black text-white bg-transparent outline-none rounded-md" placeholder="email" 
-                    required value={email} onChange={(e) => setEmail(e.target.value)}/>
-                    <p className="text-white font-bold my-2">Password</p>
-                    <input type="password" className="border-2 border-solid px-2 text-white border-black bg-transparent outline-none rounded-md"
-                    placeholder="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <p className="text-white font-bold my-2">Re-Password</p>
-                    <input type="password" className="border-2 border-solid px-2 border-black text-white bg-transparent outline-none rounded-md" placeholder="re-password" 
-                    required value={rePassword} onChange={(e) => setRePassword(e.target.value)}/>
-                </div>
-                <button className="border-2 border-solid border-black bg-white text-black rounded-md w-[5rem] my-4
-                hover:border-cyan-800 hover:bg-orange-600 ease-in-out duration-200 hover:text-white"
-                onClick={handleRegister}>Register</button>
-            </div>
+  };
+  const handleLogin = () => {
+    navigate("/login");
+  };
+  const handleBack = () => {
+    navigate("/home");
+  };
+  //   const handleMediaQueryChange = (matches) => {
+  //     // Handle media query change here
+  //     console.log("Desktop or laptop:", matches);
+  //   };
+
+  return (
+    <div className="RegisterPage">
+      <video className="AnimationBg" src={BGvid} autoPlay muted loop></video>
+      <div>
+        <div className="reciveEmail">
+          <input
+            type="text"
+            placeholder="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
+        <div className="reciveUsername">
+          <input
+            type="text"
+            placeholder="username"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-    )
+        <div className="recivePassword">
+          <input
+            type="password"
+            placeholder="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="reciveConfirmPassword">
+          <input
+            type="password"
+            placeholder="confirm password"
+            required
+            value={rePassword}
+            onChange={(e) => setRePassword(e.target.value)}
+          />
+        </div>
+
+        <button className="loginbut" onClick={handleLogin}>
+          <img src={loginbut} alt="Login Button"></img>
+        </button>
+        <button className="RegisBut" onClick={handleRegister}>
+          <img src={signupbut} alt="signup button"></img>
+        </button>
+        <button className="BackBut" onClick={handleBack}>
+          <img src={backbut} alt="back button"></img>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
