b12958b87cd773dafbaa1bbd074812235d9ea363
pppcyd <girrlwithluvv@gmail.com>
2024-04-09 13:20:29 +0700

    add sound

--- front/src/Components/users/HowToPlay.jsx
+++ front/src/Components/users/HowToPlay.jsx
@@ -3,11 +3,14 @@ import {useNavigate}  from "react-router-dom";
 import './Home.css'
 import bg from '../Assets/element/HTPbg.mp4'
 import BackBut from '../Assets/element/back.png'
+import clicksound from "../SFX/interface-button.mp3";
 
+const clickaudio = new Audio(clicksound);
 function HowToPlay(){
     const navigate = useNavigate();
     const handleBack = () => {
         navigate('/home');
+        clickaudio.play();
       }
 
       return (
