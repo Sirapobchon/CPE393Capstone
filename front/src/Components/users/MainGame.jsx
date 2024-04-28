bd098e1be2ecdc52711335122675e2e421e72020
janekmutt <janesirikarn@gmail.com>
2024-04-28 23:06:53 +0700

    Responsive (Maingame page)

--- front/src/Components/users/MainGame.jsx
+++ front/src/Components/users/MainGame.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./MainGame.css";
-import BGvid from "../Assets/MainGamebg.mp4";
+import BGvid from "../Assets/CloudBg.mp4";
 import playbut from "../Assets/element/play.png";
 import leaderbut from "../Assets/element/Leaderboard.png";
 import characterbut from "../Assets/element/character.png";
 import logoutbut from "../Assets/element/loguot.png";
-
+import rbcat from "../Assets/element/rbcat.gif";
+import rbhotdog from "../Assets/element/rbhotdog.gif";
+import dirt from "../Assets/element/dirt.png";
 
 function MainGame() {
   const navigate = useNavigate();
@@ -30,14 +32,28 @@ function MainGame() {
         <img src={playbut} alt="Play Button" className="button-img"></img>
       </button>
       <button className="Leaderbut" onClick={handleLeader}>
-        <img src={leaderbut} alt="Leaderboard Button" className="button-img"></img>
+        <img
+          src={leaderbut}
+          alt="Leaderboard Button"
+          className="button-img"
+        ></img>
       </button>
       <button className="Characterbut" onClick={handleCharacter}>
-        <img src={characterbut} alt="Character Button" className="button-img"></img>
+        <img
+          src={characterbut}
+          alt="Character Button"
+          className="button-img"
+        ></img>
       </button>
       <button className="logoutbut" onClick={handleLogout}>
         <img src={logoutbut} alt="Logout Button" className="button-img"></img>
       </button>
+      <img className="rbcat" src={rbcat} alt="rbcat"></img>
+      <img className="rbhotdog" src={rbhotdog} alt="rbhotdog"></img>
+
+      <img className="dirt1" src={dirt} alt="Login Board"></img>
+      <img className="dirt2" src={dirt} alt="Login Board"></img>
+      <img className="dirt3" src={dirt} alt="Login Board"></img>
     </div>
   );
 }
