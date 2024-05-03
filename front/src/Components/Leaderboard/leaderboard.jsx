e6612846f8e59e6b8eba23196fc38bd921e2f517
janekmutt <janesirikarn@gmail.com>
2024-05-03 21:28:51 +0700

    Responsive(Leaderboard+Gamemode)

--- front/src/Components/Leaderboard/leaderboard.jsx
+++ front/src/Components/Leaderboard/leaderboard.jsx
@@ -1,29 +1,36 @@
-import React, { useState , useEffect } from 'react';
-import {useNavigate}  from "react-router-dom";
-import './leaderboard.css'
-import bg from '../Assets/element/leaderbg2.mp4'
-import BackBut from '../Assets/element/back.png'
-import id from '../Assets/element/id.png'
-import usr from '../Assets/element/usr.png'
-import score from '../Assets/element/score.png'
-import sort from '../Assets/element/sort.png'
-import clicksound from '../SFX/interface-button.mp3'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./leaderboard.css";
+import BGvid from "../Assets/CloudBg.mp4";
+import BackBut from "../Assets/element/back.png";
+import id from "../Assets/element/id.png";
+import usr from "../Assets/element/usr.png";
+import score from "../Assets/element/score.png";
+import sort from "../Assets/element/sort.png";
+import clicksound from "../SFX/interface-button.mp3";
+import trophy from "../Assets/element/trophy.png";
+import ldbanner from "../Assets/element/ldbanner.png";
+import ldboard from "../Assets/element/ldboard.png";
+import tree from "../Assets/element/tree.png";
+import moutain2 from "../Assets/element/moutain2.png";
+import dirt from "../Assets/element/dirt.png";
+
 const clickaudio = new Audio(clicksound);
 
-function Leaderboard () {
-    const [players, setPlayers] = useState([]);
-  
-    useEffect(() => {
-      // Fetch data from ours backend server
-      async function fetchData() {
-        await fetch('http://localhost:8080/api/users/leader')
-          .then((response) => response.json())
-          .then((data) => setPlayers(data)) //I don't think this is working @Sirapobchon
-          .catch((error) => console.error('Error fetching players:', error));
-      }
+function Leaderboard() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from ours backend server
+    async function fetchData() {
+      await fetch("http://localhost:8080/api/users/leader")
+        .then((response) => response.json())
+        .then((data) => setPlayers(data)) //I don't think this is working @Sirapobchon
+        .catch((error) => console.error("Error fetching players:", error));
+    }
     fetchData();
-    }, []); // Empty dependency array ensures this effect runs once when the component mounts
-    
+  }, []); // Empty dependency array ensures this effect runs once when the component mounts
+
   const navigate = useNavigate();
   // Function to sort players by score
   const sortPlayersByScore = () => {
@@ -33,23 +40,38 @@ function Leaderboard () {
   };
   const handleBack = () => {
     clickaudio.play();
-    navigate('/mainGame');
-  }
+    navigate("/mainGame");
+  };
 
   return (
     <div className="page">
-      <video className="AnimationBg" src={bg} autoPlay muted loop></video> 
+      <video className="AnimationBg" src={BGvid} autoPlay muted loop></video>
+
+      <img className="ldboard" src={ldboard} alt="Leaderboard"></img>
+      <img className="ldbanner" src={ldbanner} alt="Leaderbanner"></img>
+      <img className="tree" src={tree} alt="tree"></img>
+      <img className="moutain2" src={moutain2} alt="moutain2"></img>
+      <img className="trophy" src={trophy} alt="trophy"></img>
+
       <button className="sort" onClick={sortPlayersByScore}>
-      <img src={sort}alt="sort button"></img> </button>
-      <button className="Backbut" onClick={handleBack}>
-                    <img src={BackBut}alt="Back button" className="button-img"></img>
-                </button>
+        <img src={sort} alt="sort button"></img>{" "}
+      </button>
+      <button className="Backbut1" onClick={handleBack}>
+        <img src={BackBut} alt="Back button" className="button-img"></img>
+      </button>
       <table>
         <thead>
           <tr>
-            <th className="Identify"> <img src={id}alt="id title"></img></th>
-            <th className="Name"><img src={usr}alt="username title"></img></th>
-            <th className="Score"><img src={score}alt="score title"></img></th>
+            <th className="Identify">
+              {" "}
+              <img src={id} alt="id title"></img>
+            </th>
+            <th className="Name">
+              <img src={usr} alt="username title"></img>
+            </th>
+            <th className="Score">
+              <img src={score} alt="score title"></img>
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -62,8 +84,12 @@ function Leaderboard () {
           ))}
         </tbody>
       </table>
+
+      <img className="dirt1" src={dirt} alt="Dirt"></img>
+      <img className="dirt2" src={dirt} alt="Dirt"></img>
+      <img className="dirt3" src={dirt} alt="Dirt"></img>
     </div>
   );
-};
+}
 
 export default Leaderboard;
