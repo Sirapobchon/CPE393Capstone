5894d6fdb7b133405387d3576a66a46d60cd501d
pppcyd <girrlwithluvv@gmail.com>
2024-05-07 21:29:35 +0700

    how to play page

--- front/src/Components/users/HowToPlay.jsx
+++ front/src/Components/users/HowToPlay.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Home.css';
-import bg from '../Assets/element/HTPbg.mp4';
+import BGvid from "../Assets/CloudBg.mp4";
 import BackBut from '../Assets/element/back.png';
+import dirt from "../Assets/element/dirt.png";
+import ldboard from "../Assets/element/htpboard.png";
+import HTPbanner from "../Assets/element/htp.png";
+import tree from "../Assets/element/tree.png";
+import rbcat from "../Assets/element/rbcat.gif";
 
 import clicksound from '../SFX/interface-button.mp3';
 const clickaudio = new Audio(clicksound);
@@ -17,10 +21,17 @@ function HowToPlay() {
 
   return (
     <div className="page">
-      <video className="AnimationBg" src={bg} autoPlay muted loop></video>
+      <video className="AnimationBg" src={BGvid} autoPlay muted loop></video>
       <button className="Backbut" onClick={handleBack}>
         <img src={BackBut} alt="Back button" className="button-img" />
       </button>
+      <img className="tree" src={tree} alt="tree"></img>
+      <img className="rbcat" src={rbcat} alt="rbcat"></img>
+      <img className="ldboard" src={ldboard} alt="Leaderboard"></img>
+      <img className="ldbanner" src={HTPbanner} alt="Leaderbanner"></img>
+      <img className="dirt1" src={dirt} alt="dirt"></img>
+      <img className="dirt2" src={dirt} alt="dirt"></img>
+      <img className="dirt3" src={dirt} alt="dirt"></img>
     </div>
   );
 }
